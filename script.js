159d3f441114153f0a7c8525dc667f05b0045fea
Supriya Reddy <121212953+Supriya-wl@users.noreply.github.com>
2023-03-20 12:23:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 //your code here!
 
+const list = document.querySelector('ul');
+let page = 1;
+
+function addItems() {
+  // Fetch data from API or add items to list
+  const items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6', 'Item 7', 'Item 8', 'Item 9', 'Item 10'];
+  items.forEach(item => {
+    const li = document.createElement('li');
+    li.textContent = item;
+    list.appendChild(li);
+  });
+}
+
+addItems();
+
+window.addEventListener('scroll', () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    page++;
+    addItems();
+  }
+});
